28b54812cb39a4a7ba9b8762b15901bea00f095f
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-04-08 13:24:01 -0600

    Mostrando pokemon y options

---
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -0,0 +1,52 @@
+import { computed, onMounted, ref } from 'vue';
+import { GameStatus, type Pokemon, type PokemonListResponse } from '../interfaces';
+import { pokemonApi } from '../api/pokemonApi';
+
+export const usePokemonGame = () => {
+  const gameStatus = ref<GameStatus>(GameStatus.Playing);
+  const pokemons = ref<Pokemon[]>([]);
+  const pokemonOptions = ref<Pokemon[]>([]);
+
+  const randomPokemon = computed(() => {
+    const randomIndex = Math.floor(Math.random() * pokemonOptions.value.length);
+    return pokemonOptions.value[randomIndex];
+  });
+  const isLoading = computed(() => pokemons.value.length === 0);
+
+  const getPokemons = async (): Promise<Pokemon[]> => {
+    const response = await pokemonApi.get<PokemonListResponse>('/?limit=151');
+    const pokemonArray = response.data.results.map((pokemon) => {
+      const urlParts = pokemon.url.split('/');
+      const id = urlParts.at(-2) ?? 0;
+
+      return {
+        name: pokemon.name,
+        id: +id,
+      };
+    });
+
+    return pokemonArray.sort(() => Math.random() - 0.5);
+  };
+
+  const getNextOptions = (howMany: number = 4) => {
+    gameStatus.value = GameStatus.Playing;
+    pokemonOptions.value = pokemons.value.slice(0, howMany);
+    pokemons.value = pokemons.value.slice(howMany);
+  };
+
+  onMounted(async () => {
+    // await new Promise((r) => setTimeout(r, 1000));
+    pokemons.value = await getPokemons();
+    getNextOptions();
+  });
+
+  return {
+    gameStatus,
+    isLoading,
+    pokemonOptions,
+    randomPokemon,
+
+    // Methods
+    getNextOptions,
+  };
+};
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,21 +1,35 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section
+    v-if="isLoading || randomPokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Espere por favor</h1>
     <h3 class="animate-pulse">Cargando pokemons</h3>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Quién es este Pokémon?</h1>
 
     <!-- Pokemon Picture -->
-    <PokemonPicture />
+    <PokemonPicture
+      :pokemon-id="randomPokemon.id"
+      :show-pokemon="gameStatus !== GameStatus.Playing"
+    />
 
     <!-- Pokemon Options -->
-    <PokemonOptions />
+    <PokemonOptions :options="options" @selected-option="onSelectedOption" />
   </section>
 </template>
 
 <script setup lang="ts">
 import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
+import { GameStatus } from '../interfaces';
+
+const { gameStatus, randomPokemon, isLoading, pokemonOptions: options } = usePokemonGame();
+
+const onSelectedOption = (value: number) => {
+  console.log(value);
+};
 </script>
